1953ba1d770a4e60663d4cc54c086b0692a280cf
JaVaWa <info@javawa.nl>
2015-02-23 23:22:58 +0100

    Update utfgrid.php

--- utfgrid.php
+++ utfgrid.php
@@ -7,7 +7,7 @@ $db = new PDO ('sqlite:' . $_GET['layer'] . '.mbtiles');
 $data = $db->prepare ('SELECT grid FROM grids WHERE zoom_level=' . $zoom . ' AND tile_column=' . $column . ' AND tile_row=' . $row);
 $data->execute();
 $grid = $data->fetchObject();
-if (isset($grid->grid)) {
+if (isset ($grid->grid)) {
 	echo $_GET['callback'] . '(' . substr(gzinflate (substr ($grid->grid, 2)), 0, -1) . ', "data": {';
 	$data = $db->prepare ('SELECT key_name,key_json FROM grid_data WHERE zoom_level=' . $zoom . ' AND tile_column=' . $column . ' AND tile_row=' . $row);
 	$data->execute();
